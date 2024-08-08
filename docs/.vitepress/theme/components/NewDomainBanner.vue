c306c0be58d1919a7bfe2f33d36d59ffe0516eab
taskylizard <75871323+taskylizard@users.noreply.github.com>
2024-08-08 11:04:55 +0000

    fix: show image banner and fixes

--- docs/.vitepress/theme/components/NewDomainBanner.vue
+++ docs/.vitepress/theme/components/NewDomainBanner.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { ref, onMounted } from "vue";
 
 const isBannerVisible = ref(true);
@@ -15,14 +15,17 @@ onMounted(() => {
   }
 });
 </script>
+
 <template>
-  <div v-if="isBannerVisible" class="bg-blue-500 text-white p-4">
+  <div v-if="isBannerVisible" class="bg-blue-500 text-white p-4 mb-5">
     <div class="flex justify-between items-center">
       <span>
         wotaku.<bold class="font-bold">moe</bold> is now
-        <a class="underline" href="https://wotaku.wiki">wotaku.wiki</a>. Bookmark and share!
+        <a class="underline" href="https://wotaku.wiki">wotaku.wiki</a>.
       </span>
+      <br />
       <button @click="closeBanner" class="text-white font-bold">✕</button>
     </div>
+    <img src="/newdomainbanner.png" alt="wotaku.wiki banner" class="mt-2 w-full h-auto" />
   </div>
 </template>
