f2b5921927ca0515b28eec5d961ea6129b8adc9e
taskylizard <75871323+taskylizard@users.noreply.github.com>
2024-08-08 09:33:15 +0000

    feat: add domain change banner

---
+++ docs/.vitepress/theme/components/NewDomainBanner.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const isBannerVisible = ref(true);
+
+const closeBanner = () => {
+  isBannerVisible.value = false;
+  localStorage.setItem("ackDomainChange", "true");
+};
+
+onMounted(() => {
+  const bannerClosed = localStorage.getItem("ackDomainChange");
+  if (bannerClosed === "true") {
+    isBannerVisible.value = false;
+  }
+});
+</script>
+<template>
+  <div v-if="isBannerVisible" class="bg-blue-500 text-white p-4">
+    <div class="flex justify-between items-center">
+      <span>
+        wotaku.<bold class="font-bold">moe</bold> is now
+        <a class="underline" href="https://wotaku.wiki">wotaku.wiki</a>. Bookmark and share!
+      </span>
+      <button @click="closeBanner" class="text-white font-bold">✕</button>
+    </div>
+  </div>
+</template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,6 @@
 import { h } from "vue";
-import Theme from "vitepress/theme";
+import { type Theme } from "vitepress";
+import DefaultTheme from "vitepress/theme";
 import "@nolebase/vitepress-plugin-git-changelog/client/style.css";
 import "./style.css";
 import { enhanceAppWithTabs } from "vitepress-plugin-tabs/client";
@@ -11,24 +12,30 @@ import Tooltip from "./components/Tooltip.vue";
 import Authors from "./components/Authors.vue";
 import Components from "@fmhy/components";
 import AnnouncementPill from "./components/AnnouncementPill.vue";
+import NewDomainBanner from "./components/NewDomainBanner.vue";
 import "virtual:uno.css";
 
 export default {
-  extends: Theme,
+  extends: DefaultTheme,
   Layout: () => {
-    return h(Theme.Layout, null, {
+    return h(DefaultTheme.Layout, null, {
+      "layout-top": () => h(NewDomainBanner),
       "sidebar-nav-after": () => h(SidebarCard),
       "home-hero-prelink": () => h(AnnouncementPill),
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     });
   },
   enhanceApp({ app, router, siteData }) {
+    // @ts-expect-error
     enhanceAppWithTabs(app);
     app.component("Button", Button);
     app.component("Authors", Authors);
     app.component("Tooltip", Tooltip);
+    // @ts-expect-error
     createMediumZoomProvider(app, router);
+    // @ts-expect-error
     app.use(Components);
+    // @ts-expect-error
     app.use(NolebaseGitChangelogPlugin, {});
   },
-};
+} satisfies Theme;
