f90e8ac2d79c94d9100be19565ed5c3f72579233
taskylizard <75871323+taskylizard@users.noreply.github.com>
2024-08-27 16:53:26 +0000

    feat(api): feedback v2

--- api/routes/index.post.ts
+++ api/routes/index.post.ts
@@ -2,11 +2,42 @@ import { fetcher } from "itty-fetcher";
 import { FeedbackSchema, getFeedbackOption } from "../../docs/.vitepress/types/Feedback";
 
 export default defineEventHandler(async (event) => {
-  const { message, page, type } = await readValidatedBody(event, FeedbackSchema.parseAsync);
+  const { message, page, type, heading } = await readValidatedBody(
+    event,
+    FeedbackSchema.parseAsync,
+  );
   const env = useRuntimeConfig(event);
 
-  let description = `${message}\n\n`;
-  if (page) description += `**Page:** \`${page}\``;
+  const fields = [
+    {
+      name: "Page",
+      value: page,
+      inline: true,
+    },
+    {
+      name: "Message",
+      value: message,
+      inline: false,
+    },
+  ];
+
+  if (heading) {
+    fields.unshift({
+      name: "Section",
+      value: heading,
+      inline: true,
+    });
+  }
+
+  // FIXME: somehow this is not working, but it worked before
+  // const path = 'feedback'
+  //
+  // const { success } = await env.MY_RATE_LIMITER.limit({ key: path })
+  // if (!success) {
+  //   return new Response('429 Failure – global rate limit exceeded', {
+  //     status: 429
+  //   })
+  // }
 
   await fetcher()
     .post(env.WEBHOOK_URL, {
@@ -15,8 +46,8 @@ export default defineEventHandler(async (event) => {
       embeds: [
         {
           color: 3447003,
-          title: getFeedbackOption(type).label,
-          description,
+          title: getFeedbackOption(type)!.label,
+          fields,
         },
       ],
     })
