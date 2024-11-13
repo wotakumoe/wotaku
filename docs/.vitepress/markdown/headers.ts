101b5ebbb557068ed97302c2a1f519fdf7ac9183
c032 <git@c032.dev>
2024-11-13 17:11:17 +0000

    Fix indexing of <h2> sections

--- docs/.vitepress/markdown/headers.ts
+++ docs/.vitepress/markdown/headers.ts
@@ -18,13 +18,48 @@ import type { MarkdownRenderer } from 'vitepress'
 const excluded = ['Credits']
 
 export const headersPlugin = (md: MarkdownRenderer) => {
-  // Add the Feedback component after the heading and close the container
-  md.renderer.rules.heading_close = (tokens, idx, options, env, self) => {
+  // Add the Feedback component in the heading, before the link.
+  //
+  // Adding it after the link is closed prevents vitepress from properly
+  // indexing the file's content.
+
+  md.renderer.rules.heading_open = (tokens, idx, options, env, self) => {
     const result = self.renderToken(tokens, idx, options)
-    const heading = tokens[idx - 1]
+
+    const idxClose =
+      idx +
+      tokens.slice(idx).findIndex((token) => token.type === 'heading_close')
+    if (idxClose <= idx) return result
+
     const level = tokens[idx].tag.slice(1)
     if (excluded.includes(env.frontmatter.title) || level !== '2') return result
 
-    return `<Feedback heading="${heading.content}" />${result}`
+    // Find the token for the link.
+    //
+    // The token after `heading_open` contains the link as a child token.
+    const children = tokens[idx + 1].children || []
+    const linkOpenToken = children.find((c) => c.type === 'link_open')
+    if (!linkOpenToken) return result
+
+    const heading = tokens[idxClose - 1]
+
+    linkOpenToken.meta = linkOpenToken.meta || {}
+    linkOpenToken.meta.feedback = {
+      heading: heading.content
+    }
+
+    return result
+  }
+
+  md.renderer.rules.link_open = (tokens, idx, options, env, self) => {
+    const result = self.renderToken(tokens, idx, options)
+
+    const meta = tokens[idx].meta
+    if (!meta || !meta.feedback) return result
+
+    const heading = meta.feedback.heading || ''
+    if (!heading) return result
+
+    return `<Feedback heading="${heading}" />${result}`
   }
 }
