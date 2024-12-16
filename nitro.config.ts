b311d324adb470a85c7658ed24147f6515ade339
taskylizard <75871323+taskylizard@users.noreply.github.com>
2024-12-16 15:44:32 +0000

    chore: add nitropack compatdate

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,12 +1,13 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
+  compatibilityDate: '2024-12-16',
   runtimeConfig: {
-    WEBHOOK_URL: process.env.WEBHOOK_URL,
+    WEBHOOK_URL: process.env.WEBHOOK_URL
   },
-  srcDir: "api",
+  srcDir: 'api',
   routeRules: {
-    "/": {
-      cors: false,
-    },
-  },
-});
+    '/': {
+      cors: false
+    }
+  }
+})
