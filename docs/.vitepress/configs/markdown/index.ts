a38e8550f39d941263f24c6785f3ed88673e8746
Duck <57977673+woducku@users.noreply.github.com>
2025-03-20 18:08:52 +0600

    Update index.ts

--- docs/.vitepress/configs/markdown/index.ts
+++ docs/.vitepress/configs/markdown/index.ts
@@ -360,7 +360,7 @@ const tooltips: {
   demo: { content: 'Demo tooltips for start page' },
   mirai: { content: 'Syncs the Mangayomi repository and includes their own plugins' },
   megarepo: { content: 'Adds all the repo present in /recloudstream/cs-repos' },
-  dab: { content: 'Although it mentions DSD/DXD (in about), it does not download those.' }
+  dab: { content: 'It downloads from qobuz streaming platform, which only supports flac. So, you wont be able to download other formats mentioned in the about page.' }
 }
 
 export function configureMarkdown(md: MarkdownRenderer) {
