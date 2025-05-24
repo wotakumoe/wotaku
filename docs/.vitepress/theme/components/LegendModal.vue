6b91901a7d73a8ea37db58f34773b51f4190a93f
Duck <57977673+woducku@users.noreply.github.com>
2025-05-24 22:58:40 +0600

    Add all the legends

--- docs/.vitepress/theme/components/LegendModal.vue
+++ docs/.vitepress/theme/components/LegendModal.vue
@@ -27,21 +27,40 @@ const sections = [
     title: 'Frequent',
     items: [
       { icon: 'i-twemoji-glowing-star', label: 'Favorite' },
+      { icon: 'i-mdi-puzzle', label: 'Extension / Plugin' },
       { icon: 'i-material-symbols-directions-alt', label: 'Proxies' },
+      { icon: 'i-twemoji-no-one-under-eighteen', label: 'R18' },
       { icon: 'i-material-symbols-person-add', label: 'Needs account' },
       { icon: 'i-material-symbols-help', label: 'Help / Docs' },
-      { icon: 'i-mdi-arrow-right-bold', label: 'Related' }
+      { icon: 'i-mdi-arrow-right-bold', label: 'Related' },
+
+    ]
+  },
+  {
+    title: 'System',
+    items: [
+      { icon: 'i-simple-icons-windows', label: 'Windows' },
+      { icon: 'i-simple-icons-android', label: 'Android' },
+      { icon: 'i-simple-icons-apple', label: 'Apple' },
+      { icon: 'i-simple-icons-linux', label: 'Linux' },
+      { icon: 'i-simple-icons-freebsd', label: 'BSD' },
+      { icon: 'i-material-symbols-terminal-rounded', label: 'CLI / TUI' },
+      { icon: 'i-simple-icons-firefoxbrowser', label: 'Firefox' },
+      { icon: 'i-simple-icons-googlechrome', label: 'Chromium' },
+      { icon: 'i-mdi-web', label: 'Web' }
     ]
   },
   {
     title: 'Type',
     items: [
-      {
-        icon: 'i-material-symbols-cloud-download-outline-rounded',
-        label: 'Online / DDL'
-      },
+      { icon: 'i-material-symbols-play-circle-outline', label: 'Stream' },
+      { icon: 'i-material-symbols-cloud-download-outline-rounded', label: 'Online / DDL' },
       { icon: 'i-lucide-magnet', label: 'Torrent / p2p' },
-      { icon: 'i-iconoir-floppy-disk', label: 'Local' }
+      { icon: 'i-iconoir-floppy-disk', label: 'Local file' },
+      { icon: 'i-material-symbols-package-2-outline', label: 'Batch Download' },
+      { icon: 'i-lucide-file', label: 'Single Page Mode' },
+      { icon: 'i-lucide-book-open', label: 'Double Page Mode' },
+      { icon: 'i-lucide-scroll', label: 'Long-Strip Mode' }
     ]
   },
   {
@@ -55,17 +74,73 @@ const sections = [
   },
   {
     title: 'Source',
-    items: [{ icon: 'i-material-symbols-lock-outline', label: 'Closed Source' }]
+    items: [
+      { icon: 'i-simple-icons-github', label: 'GitHub' },
+      { icon: 'i-simple-icons-gitlab', label: 'GitLab' },
+      { icon: 'i-simple-icons-codeberg', label: 'Codeberg' },
+      { icon: 'i-simple-icons-kde', label: 'KDE' },
+      { icon: 'i-simple-icons-gnome', label: 'GNOME' },
+      { icon: 'i-simple-icons-gitea', label: 'Gitea' },
+      { icon: 'i-material-symbols-lock-outline', label: 'Closed Source' }
+    ]
+  },
+  {
+    title: 'Platform',
+    items: [
+      { icon: 'i-simple-icons-discord', label: 'Discord' },
+      { icon: 'i-simple-icons-4chan', label: '4chan' },
+      { icon: 'i-simple-icons-twitter', label: 'Twitter' },
+      { icon: 'i-simple-icons-telegram', label: 'Telegram' },
+      { icon: 'i-simple-icons-myanimelist', label: 'MyAnimeList' },
+      { icon: 'i-simple-icons-anilist', label: 'AniList' },
+      { icon: 'i-simple-icons-kitsu', label: 'Kitsu' },
+      { icon: 'i-simple-icons-simkl', label: 'Simkl' }
+    ]
+  },
+  {
+    title: 'Storage',
+    items: [
+      { icon: 'i-simple-icons-googledrive', label: 'Google Drive' },
+      { icon: 'i-simple-icons-mega', label: 'MEGA' },
+      { icon: 'i-simple-icons-mediafire', label: 'MediaFire' },
+      { icon: 'i-mdi-snail', label: 'Other Host' }
+    ]
+  },
+  {
+    title: 'Region',
+    items: [
+      { icon: 'i-twemoji-flag-japan', label: 'Japan' },
+      { icon: 'i-twemoji-flag-south-korea', label: 'Korea' },
+      { icon: 'i-twemoji-flag-china', label: 'China' }
+    ]
+  },
+  {
+    title: 'Code',
+    items: [
+      { icon: 'i-simple-icons-javascript', label: 'JavaScript' },
+      { icon: 'i-mdi-language-css3', label: 'CSS' },
+      { icon: 'i-simple-icons-python', label: 'Python' },
+      { icon: 'i-simple-icons-docker', label: 'Docker' }
+    ]
   },
   {
     title: 'Others',
     items: [
       { icon: 'i-material-symbols-science', label: 'Nightly build' },
       { icon: 'i-lucide-mail', label: 'Feedback' },
-      { icon: 'i-material-symbols-info-outline-rounded', label: 'More Info' }
+      { icon: 'i-material-symbols-info-outline-rounded', label: 'More Info' },
+      { icon: 'i-ic-round-looks-two', label: 'Alternative' },
+      { icon: 'i-simple-icons-rss', label: 'RSS Feed' },
+      { icon: 'i-mdi-airplane', label: 'Airgap / Transfer' },
+      { icon: 'i-twemoji-check-mark-button', label: 'Yes / Available' },
+      { icon: 'i-twemoji-cross-mark', label: 'No / Unavailable' },
+      { icon: 'i-twemoji-globe-showing-asia-australia', label: 'Global' },
+      { icon: 'i-mdi-closed-caption', label: 'Subtitles / CC' },
+      { icon: 'i-mdi-hulu', label: 'Hardsubs' }
     ]
   }
 ]
+
 </script>
 
 <template>
