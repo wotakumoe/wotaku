b99618ee41e0ec8693413cb17eaa4c6099f5c5cb
Duck <57977673+woducku@users.noreply.github.com>
2026-05-10 20:34:53 +0600

    Update Collapsible.vue
    Using lucide arrow instead of unicode arrow

--- docs/.vitepress/theme/components/Collapsible.vue
+++ docs/.vitepress/theme/components/Collapsible.vue
@@ -10,7 +10,31 @@ const open = ref(false)
 
 <template>
   <details class="details custom-block" :open="open || undefined" @toggle="open = ($event.target as HTMLDetailsElement).open">
-    <summary>{{ title || 'Details' }}</summary>
+    <summary>
+      <span v-if="open" class="i-lucide-arrow-down collapsible-icon" />
+      <span v-else class="i-lucide-arrow-right collapsible-icon" />
+      {{ title || 'Details' }}
+    </summary>
     <slot />
   </details>
 </template>
+
+<style scoped>
+summary {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  gap: 0.4em;
+  cursor: pointer;
+}
+
+summary::-webkit-details-marker {
+  display: none;
+}
+
+.collapsible-icon {
+  width: 1em;
+  height: 1em;
+  flex-shrink: 0;
+}
+</style>
