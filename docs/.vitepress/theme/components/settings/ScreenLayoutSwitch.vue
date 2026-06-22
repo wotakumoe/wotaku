d9bd3e965b9fe47eab56dbf2b130f83401a16605
Duck <57977673+woducku@users.noreply.github.com>
2026-06-22 09:31:48 +0600

    phone layout menu

--- docs/.vitepress/theme/components/settings/ScreenLayoutSwitch.vue
+++ docs/.vitepress/theme/components/settings/ScreenLayoutSwitch.vue
@@ -1,41 +1,5 @@
 <script setup lang="ts">
-import { NuInputHorizontalRadioGroup } from '@nolebase/ui'
-import { computed } from 'vue'
-
 import MenuTitle from './MenuTitle.vue'
-
-import { LayoutMode } from '../../constants'
-
-const fieldOptions = computed(() => [
-  {
-    value: LayoutMode.FullWidth,
-    title: 'Expand all',
-    ariaLabel: 'Expand all',
-    icon: 'i-icon-park-outline:full-screen-one',
-    name: 'VitePress Nolebase Enhanced Readabilities Layout Mode Checkbox'
-  },
-  {
-    value: LayoutMode.SidebarWidthAdjustableOnly,
-    title: 'Expand sidebar with adjustable values',
-    ariaLabel: 'Expand sidebar with adjustable values',
-    icon: 'i-icon-park-outline:full-screen-two',
-    name: 'VitePress Nolebase Enhanced Readabilities Layout Mode Checkbox'
-  },
-  {
-    value: LayoutMode.BothWidthAdjustable,
-    title: 'Expand all with adjustable values',
-    ariaLabel: 'Expand all with adjustable values',
-    icon: 'i-icon-park-outline:full-screen',
-    name: 'VitePress Nolebase Enhanced Readabilities Layout Mode Checkbox'
-  },
-  {
-    value: LayoutMode.Original,
-    title: 'Original width',
-    ariaLabel: 'Original width',
-    icon: 'i-icon-park-outline:overall-reduction',
-    name: 'VitePress Nolebase Enhanced Readabilities Layout Mode Checkbox'
-  }
-])
 </script>
 
 <template>
@@ -56,11 +20,5 @@ const fieldOptions = computed(() => [
     >
       <span text-xs>No available layout can be switched in mobile screen.</span>
     </div>
-    <NuInputHorizontalRadioGroup
-      bg="$vp-nolebase-enhanced-readabilities-menu-background-color"
-      text="sm $vp-nolebase-enhanced-readabilities-menu-text-color"
-      :options="fieldOptions"
-      disabled
-    />
   </div>
 </template>
--- docs/.vitepress/theme/components/settings/ScreenMenu.vue
+++ docs/.vitepress/theme/components/settings/ScreenMenu.vue
@@ -1,28 +1,26 @@
 <script setup lang="ts">
 import { useMounted } from '@vueuse/core'
+import { useData } from 'vitepress'
+import VPSocialLinks from 'vitepress/dist/client/theme-default/components/VPSocialLinks.vue'
 
 import AccentColor from './AccentColor.vue'
 import BlurEffects from './BlurEffects.vue'
-import MenuTitle from './MenuTitle.vue'
-import ScreenLayoutSwitch from './ScreenLayoutSwitch.vue'
-import ScreenSpotlight from './ScreenSpotlight.vue'
+import ScreenAppearance from './ScreenAppearance.vue'
 
 const mounted = useMounted()
+const { theme } = useData()
 </script>
 
 <template>
   <div v-if="mounted" space-y-2 class="VPNolebaseEnhancedReadabilitiesMenu">
-    <MenuTitle title="Enhanced Readability" aria-label="Enhanced Readability">
-      <template #icon>
-        <span i-icon-park-outline:setting-two mr-1 aria-hidden="true" />
-      </template>
-    </MenuTitle>
-    <div flex="~ col" pl-4 space-y-2>
-      <ScreenLayoutSwitch />
-      <ScreenSpotlight />
-      <BlurEffects />
-      <AccentColor />
-    </div>
+    <BlurEffects />
+    <ScreenAppearance />
+    <AccentColor />
+    <VPSocialLinks
+      v-if="theme.socialLinks"
+      :links="theme.socialLinks"
+      class="screen-menu-social-links"
+    />
   </div>
 </template>
 
